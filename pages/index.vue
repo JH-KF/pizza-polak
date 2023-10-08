c63b10440511d8bc475a9a53177eeb6e608804ad
Kevin Fabre <me@kevinfabre.com>
2023-10-08 22:49:30 +0200

    fix: content

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,12 @@ import { groupBy } from "lodash";
 import Pizza from "~/lib/Pizza";
 import { CATEGORY } from "~/lib/constants";
 
-const rawPizza = await queryContent("/pizza").findOne();
+const { data } = await useAsyncData("home", () =>
+  queryContent("/pizza").findOne()
+);
+
 const pizzaList = computed(() =>
-  rawPizza.body.map((p) => new Pizza(p)).filter((p) => p.visible)
+  data.value.body.map((p) => new Pizza(p)).filter((p) => p.visible)
 );
 const pizzaByCategory = computed(() => groupBy(pizzaList.value, "category"));
 </script>
