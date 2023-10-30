d69b5d943dccf4848293ab025620d4ced8b6bf0c
KevinFabre <48985996+KevinFabre@users.noreply.github.com>
2023-10-30 14:07:35 +0000

    fix: update siteName

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "Pizza Polak",
+  siteName: "Polak Pizza",
   siteDescription:
     "Pizza maison cuites au feu à bois. A venir cherchez à Farschviller.",
   siteUrl: "https://pizza-polak.fr",
