95d36785528296f2f81de240c05ace585ead6f24
Kevin Fabre <me@kevinfabre.com>
2024-02-03 11:30:41 +0100

    style: update hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,20 +1,18 @@
 <template>
   <section class="hero">
-    <template v-if="!!firstOpeningDate">
-      <h3>Prochaine ouverture le</h3>
-      <div class="opening_date">{{ firstOpeningDate }}</div>
-    </template>
-
+    <h3>Prochaine ouverture le</h3>
+    <div class="opening_date">
+      {{ !!firstOpeningDate ? firstOpeningDate : "..." }}
+    </div>
     <div>🕐 18:30h à 20:30h</div>
-
     <div>
       🚩
       <a href="https://maps.app.goo.gl/CXQxjXCXCQpaASWq6" target="_blank"
         >1b rue Saint-Jean, 57450 Farschviller</a
       >
     </div>
-
     <!-- <div>📞 <a href="tel:+33600000000"> 06 00 00 00 00</a></div> -->
+    <div>💰 Paiement uniquement en espèces</div>
   </section>
 </template>
 
